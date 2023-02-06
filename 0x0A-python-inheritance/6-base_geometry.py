3ebba470e7fa27fe582215ddd31038c4e50d55d2
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-02-06 05:22:26 -0800

    task 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,7 +5,7 @@ Contains the class BaseGeometry
 
 
 class BaseGeometry:
-    """A base geometry class""" 
+    """A base geometry class"""
 
     def area(self):
         """raises an Exception with the message area() is not implemented"""
