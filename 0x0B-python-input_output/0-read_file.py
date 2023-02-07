c4b74ef0892c2a6c08373bd45b32c54b6d8ac41b
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-02-07 01:24:44 -0800

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@ contains the function read_file
 
 
 def read_file(filename=""):
-    """ reads a text file (UTF8) and prints it to stdout"""
+    """"reads a text file (UTF8) and prints it to stdout"""
     with open(filename, "r", encoding-"utf-8") as f:
         print(f.read(), end="")
