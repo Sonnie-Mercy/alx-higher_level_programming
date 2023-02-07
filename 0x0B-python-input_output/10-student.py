295c350081d50e0c6850516c2189921ba5c08ae0
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-02-07 03:25:10 -0800

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""
-module that contains a class called student
-"""
+""" Student to JSON """
 
 
 class Student:
-    """a class that defines student by first name, last name and age"""
-def __init__(self, first_name, last_name, age):
-    """initializes student"""
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+    """ New class student """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
-        """ Returns a dictionary representation of a Student instance."""
         if attrs is None:
-            return self.__dict__
-        mydict = {}
-        for key in attrs:
-            if key in self.__dict__.keys():
-                mydict[key] = self.__dict__.get(key)
-        return mydict
+            return (self.__dict__)
+        else:
+            dic = {}
+            for nm in attrs:
+                if hasattr(self, nm):
+                    dic[nm] = getattr(self, nm)
+            return (dic)
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-module that contains a class called student
+Contains the class "Student"
 """
 
 
 class Student:
-    """a class that defines student by first name, last name and age"""
-def __init__(self, first_name, last_name, age):
-    """initializes student"""
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+    """Representation of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
-        """ Returns a dictionary representation of a Student instance."""
+        """returns a dictionary representation of a Student instance"""
         return self.__dict__
