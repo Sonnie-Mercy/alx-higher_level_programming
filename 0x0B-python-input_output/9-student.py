1a8fdd97c007ab065090203c6769908f8253c534
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-02-07 03:05:11 -0800

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+module that contains a class called student
+"""
+
+
+class Student:
+    """a class that defines student by first name, last name and age"""
+
+
+def __init__(self, first_name, last_name, age):
+    self.first_name = first name
+    self.last_name = last name
+    self.age = age
+
+    def to_json(self):
+        """ Returns a dictionary representation of a Student instance."""
+        return self.__dict__
