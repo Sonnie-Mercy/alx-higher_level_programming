3b61fccd51c177fd9eecc1543793677142d9e936
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-02-07 02:56:57 -0800

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,9 +4,6 @@ contains the function class_to_json
 """
 
 
-import json
-
-
 def class_to_json(obj):
     """returns the dictionary description with simple data structure
     for JSON serialization of an object"""
