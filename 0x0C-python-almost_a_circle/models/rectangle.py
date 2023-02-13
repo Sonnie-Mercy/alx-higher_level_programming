428e624f9d22c9aadd3dc492e32a9366b9b0fe39
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-02-13 09:11:58 -0800

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,20 +17,11 @@ class Rectangle(base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """initializes the rectangle class
-        Args:
-            - __width: width
-            - __height: height
-            - __x: position
-            - __y: position
-            - id: id
-            """
-
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
         @property
         """retrieves the width attribute"""
