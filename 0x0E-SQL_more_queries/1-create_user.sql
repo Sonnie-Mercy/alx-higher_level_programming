42a6d6c0891d680dbd9e20cbcd9478bf5771893e
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-03-15 04:21:41 -0700

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,4 @@
---creates MySQL server user user_0d_1
--- has all permissions
--- password is user_0d_1_pwd
-CREATE USER IF NOT EXISTS `user_0d_1`@`localhost`
-IDENTIFIED BY `user_0d_1_pwd`;
-GRANT ALL  PRIVILLEGES ON *.* TO `user_0d_1`@`localhost`;
+-- creates MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL  PRIVILLEGES ON *.* TO 'user_0d_1'@'localhost';
