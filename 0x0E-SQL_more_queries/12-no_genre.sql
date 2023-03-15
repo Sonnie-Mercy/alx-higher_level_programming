ffaec641135d79c425340b53299daba3d47fc7dd
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-03-15 10:54:17 -0700

    task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT s.title, sg.genre_id
 FROM tv_shows s
-RIGHT JOIN tv_show_genres sg ON s.id = sg.show_id
+LEFT OUTER JOIN tv_show_genres sg ON s.id = sg.show_id
 WHERE sg.show_id is NULL
 ORDER BY s.title, sg.genre_id;
