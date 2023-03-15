92b483151ee03bfaba52eefff899ae6311fec627
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-03-15 04:08:38 -0700

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,6 +2,6 @@
 -- has all permissions
 -- password is user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL  PRIVILLEGES ON *.* TO 'user_0d_1';
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL  PRIVILLEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILLEGES;
