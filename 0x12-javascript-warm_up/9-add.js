a5d47599fb90b764185a36f7bef1a0682fa41c98
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-04-11 12:24:43 -0700

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-function add (a, b) {
-  console.log(parseInt(a)+ parseInt(b));
+function add(a, b) {
+  return a + b;
 }
-console.log(add(process.argv[2], process.argv[3]));
+console.log(add(parseInt(process.argv[2]), (parseInt(process.argv[3]))));
