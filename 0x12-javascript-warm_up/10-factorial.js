f9062262a3406d220399f3f8043884a5dae93aec
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-04-11 10:58:11 -0700

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function factorial (a) {
+  if (isNaN(a)) {
+    return 1;
+  } else if (a === 0) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
+}
+console.log(factorial(process.argv[2]));
