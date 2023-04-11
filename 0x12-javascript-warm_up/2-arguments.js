6cd1581068b653e77c27c4be7d1cf180237d1011
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-04-11 06:19:55 -0700

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@ const args = process.argv.slice(2);
 const theArgs = args.length;
 
 if (theArgs === 0) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (theArgs === 1) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
