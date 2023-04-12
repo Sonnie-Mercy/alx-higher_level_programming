668e04ee76640869b8a3f4d6a3b4016d8278fc1f
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-04-12 04:46:55 -0700

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let row = '';
+    for (let i = 0; i < this.width; i++) {
+      row += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+};
