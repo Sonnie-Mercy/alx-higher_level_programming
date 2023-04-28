dd9807c29ded34e191ebcda604c00cedecd7c193
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-04-28 14:58:59 -0700

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL and
+displays the body of the response"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    resp = requests.get(url)
+
+    if resp.status_code >= 400:
+        print("Error code: {}".format(resp.status_code))
+    else:
+        print(resp.text)
