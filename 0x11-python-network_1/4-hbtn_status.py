7b362a83fa96e2f2e17083bd49a024677c20232c
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-04-28 14:23:57 -0700

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    resp = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(resp.text)))
+    print("\t- content: {}".format(resp.text))
