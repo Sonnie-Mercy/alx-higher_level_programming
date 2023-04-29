8971c6e1211d5a488cdc6f76a98cdb50613ddb06
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-04-29 02:16:30 -0700

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,8 +10,9 @@ def find_peak(list_of_integers):
     while first <= last:
         mid = (first + last) // 2
 
-        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1])
-        and \ (mid == n - 1 or list_of_integers[mid] >=
+        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]):
+            return list_of_integers[mid]
+        if (mid == n - 1 or list_of_integers[mid] >=
                 list_of_integers[mid + 1]):
             return list_of_integers[mid]
 
