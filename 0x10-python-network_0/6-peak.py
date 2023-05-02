933ed52681e0b592d5f9b167934625681105d733
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-05-02 00:40:57 -0700

    peak finding

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,12 +13,12 @@ def find_peak(list_of_integers):
         return list_of_integers[0]
     elif n == 2:
         return max(list_of_integers)
-
-    mid = n // 2
-    if list_of_integers[mid] >= list_of_integers[mid-1] and \
-            list_of_integers[mid] >= list_of_integers[mid+1]:
-        return list_of_integers[mid]
-    elif list_of_integers[mid] < list_of_integers[mid-1]:
-        return find_peak(list_of_integers[:mid])
     else:
-        return find_peak(list_of_integers[mid+1:])
+        mid = n // 2
+        if list_of_integers[mid] >= list_of_integers[mid-1] and \
+                list_of_integers[mid] >= list_of_integers[mid+2]:
+            return list_of_integers[mid]
+        elif list_of_integers[mid] < list_of_integers[mid-1]:
+            return find_peak(list_of_integers[:mid])
+        else:
+            return find_peak(list_of_integers[mid+1:])
