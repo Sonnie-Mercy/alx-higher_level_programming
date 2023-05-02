49956db054865376fb7a91e193f47d40236762b4
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-05-02 01:44:19 -0700

    task 7

---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# script sends request to a URL and displays the status code of the response
+curl -s -o /dev/null -w "%{http_code}" "$1"
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a JSON post request url and returns the body of the response
-curl -s -X POST -H  "Content-Type: application/JSON" -d "@$2" "$1"
+curl -s -X POST -H  "Content-Type: application/json" -d "@$2" "$1"
