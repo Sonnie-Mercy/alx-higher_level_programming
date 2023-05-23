20ca0649a937fe3cd91da3a4f299e1e91f92c662
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-05-23 10:27:29 -0700

    semistandard fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
 
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
+    return;
+  }
+
+  if (response.statusCode === 200) {
+    const results = JSON.parse(body).results;
+    const count = results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0);
+
+    console.log('Number of movies with Wedge Antilles:', count);
   } else {
-    console.log(JSON.parse(body).title);
+    console.error('Unable to fetch movie data.');
   }
 });
