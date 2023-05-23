2362024c8d55eda321b5d63f82eca31354ab5faa
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-05-23 10:13:41 -0700

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,23 +2,11 @@
 
 const request = require('request');
 
-const url = process.argv[2];
-const charId = 18;
-
-request.get(url, function (error, response, body) {
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
   if (error) {
     console.error(error);
-    return;
-  }
-
-  if (response.statusCode === 200) {
-    const films = JSON.parse(body).results;
-    const movies = films.filter((film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${charId}/`)
-    );
-
-    console.log('Number of movies with Wedge Antilles:', movies.length);
   } else {
-    console.error('Unable to fetch movie data.');
+    console.log(JSON.parse(body).title);
   }
 });
