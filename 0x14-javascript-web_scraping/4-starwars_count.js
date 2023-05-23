24f82eaf63eca3a73a03149c5389646117eeee55
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-05-23 10:31:04 -0700

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,16 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
-
-request(url, function (error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
     return;
   }
 
-  if (response.statusCode === 200) {
-    const results = JSON.parse(body).results;
-    const count = results.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1
-        : count;
-    }, 0);
-
-    console.log('Number of movies with Wedge Antilles:', count);
-  } else {
-    console.error('Unable to fetch movie data.');
-  }
+  const results = JSON.parse(body).results;
+  console.log(results.reduce((count, movie) => {
+    return movie.characters.find((character) => character.endsWith('/18/'))
+      ? count + 1
+      : count;
+  }, 0));
 });
