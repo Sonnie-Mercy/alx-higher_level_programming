ad06abb5fac75b1d144b6c3b683bebeea0c0e838
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-05-23 05:55:57 -0700

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,8 +4,5 @@ const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (error) {
   if (error) {
     console.error(error);
-    return;
   }
-
-  console.log('File written successfully.');
 });
