064a9b77f980f8783196f93472a76c783513ee96
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-05-23 09:49:14 -0700

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/${process.argv[2]}';
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request.get(apiUrl, function (error, response, body) {
   if (error) {
     console.error(error);
