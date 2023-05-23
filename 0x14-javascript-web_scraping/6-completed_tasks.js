67139f0bed2d7a6bd76ced7d0df1030a6855e89a
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-05-23 10:58:30 -0700

    semistandard fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,7 +6,7 @@ request(process.argv[2], function (err, response, body) {
   if (err == null) {
     const resp = {};
     const todos = JSON.parse(body);
-    todos.forEach(function(todo) {
+    todos.forEach(function (todo) {
       if (todo.completed) {
         if (resp[todo.userId] === undefined) {
           resp[todo.userId] = 0;
