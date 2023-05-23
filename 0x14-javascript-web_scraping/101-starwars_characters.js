46a7a8b50a8ca99f0716606ca9a7abdda0435881
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-05-23 11:11:52 -0700

    final commit

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,16 +6,24 @@ const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(apiUrl, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
+  if (!error) {
     const characters = JSON.parse(body).characters;
-
-    characters.forEach(function (characterUrl) {
-      request(characterUrl, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-        }
-      });
-    });
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+
+  const characterUrl = characters[index];
+  request(characterUrl, function (error, response, body) {
+    if (!error) {
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    }
+
+    printCharacters(characters, index + 1);
+  });
+}
