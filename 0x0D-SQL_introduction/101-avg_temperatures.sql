0dafd8ce92257a8d0526818623c0d59e8b295b63
root <ndirangumercy4@gmail.com>
2023-07-18 05:49:26 -0700

    task 18

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- import dump and convert temperature to Farenheit
-SELECT city, AVG(value) FROM temperatures GROUP BY city ORDER BY 2 DESC;
+SELECT city, AVG(value * 9/5) + 32) FROM temperatures GROUP BY city ORDER BY 2 DESC;
