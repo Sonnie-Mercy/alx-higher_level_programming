5e5b4c87052d031aeda5a5e24e615054ba9b4a46
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-08-14 13:53:08 +0000

    task 7

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const lines = [
-"C is fun", "Python is cool", "JavaScript is amazing"
+  'C is fun', 'Python is cool', 'JavaScript is amazing'
 ];
 for (const line of lines) {
-console.log(line);
+  console.log(line);
 }
