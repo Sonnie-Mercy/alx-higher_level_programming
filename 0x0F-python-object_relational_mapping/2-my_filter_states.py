6dafc8b4ccd41e5d377a212855f611e386fcfa79
root <ndirangumercy4@gmail.com>
2023-08-18 03:16:02 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-
-"""displays all values in the states table that match argument"""
-
+"""
+Script that displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
 
 import sys
 import MySQLdb
 
 
-def display_states(username, password, database, search_name):
+def filter_states(username, password, database, search_name):
     """
-    displays values where name matches argument"""
+    Displays values in the states table where name matches the argument.
+    """
     db = MySQLdb.connect(
-            host="localhost",
-            user=username,
-            passwd=password,
-            db=database,
-            port=3306
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
     )
 
     cursor = db.cursor()
-    query = "SELECT * FROM states where name LIKE %s ORDER BY states.id ASC"
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
     cursor.execute(query, (search_name,))
 
     rows = cursor.fetchall()
@@ -39,4 +41,4 @@ if __name__ == "__main__":
     database = sys.argv[3]
     search_name = sys.argv[4]
 
-    display_states(username, password, database, search_name)
+    filter_states(username, password, database, search_name)
