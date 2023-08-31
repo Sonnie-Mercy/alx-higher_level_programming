ad069b2f39afda559a9e39d38f481c3b8a662d08
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-08-31 09:19:22 -0700

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script that takes url, sends GET request and displays the body of response
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-[ "$response" -eq 200 ] && curl -s "$1"
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ "$response" -eq 200 ] && curl -s "$1"
