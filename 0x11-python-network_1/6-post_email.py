5d884b1e04a4f6d9750ce9b0c6560a02ddee8997
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-09-01 08:01:34 -0700

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,9 +7,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    try:
-        response = requests.post(url, data={'email': email})
-        print("Your email is:", email)
-        print(response.text)
-    except Exception as e:
-        print(e)
+    response = requests.post(url, data={'email': email})
+    print("Your email is:", email)
+    print(response.text)
