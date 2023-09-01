fb30c481a063c9c91c0f32d02e3b4ff06cfe7810
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-09-01 07:02:40 -0700

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    response = requests.post(url, data={'email'})
+    response = requests.post(url, data={'email': email})
     print("your email is:", email)
     print(response.text)
