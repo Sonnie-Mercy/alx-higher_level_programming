051abeb6a1f3fbc852581149e8cd9ba62802038f
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-09-01 05:49:01 -0700

    task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""taskes URL, sends request to URL and displays
+the X-Request-ID found in the header"""
+import urllib.request
+import sys
+
+url =sys.argv[1]
+with urllib.request.urlopen(url) as response:
+    Id = response.getheader('X-Request-Id')
+    if Id:
+        print(Id)
