5acd6f2aeb3c2e19bf5d896736e191b3c53611ff
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-09-01 07:00:11 -0700

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""using email and a POST request using the requests library"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    response = requests.post(url, data={'email'})
+    print("your email is:", email)
+    print(response.text)
