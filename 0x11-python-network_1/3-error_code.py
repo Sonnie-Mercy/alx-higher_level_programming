da1b756720b0f6177ec50c7a5de210e07a262ff3
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-09-01 06:40:05 -0700

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""takes URL, sends request to URL, displays body of response
+manages urllib.error.HTTPError exceptions"""
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urlib.error.HTTPerror as e:
+        print("Error code:", e.code)
