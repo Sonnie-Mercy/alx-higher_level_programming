74b6a90e4f5e84f4106cfc893e9cc42e36e9f1d0
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-09-01 06:28:24 -0700

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,9 @@ the X-Request-ID found in the header"""
 import urllib.request
 import sys
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    Id = response.getheader('X-Request-Id')
-    if Id:
-        print(Id)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        Id = response.getheader('X-Request-Id')
+        if Id:
+            print(Id)
